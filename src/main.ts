4f2d482c2cabdc0c647b53cf3fbfc6f6e20665ec
Federico Juretich <fedejuret@gmail.com>
2021-11-14 17:22:34 -0300

    Adding .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HandlerFilter } from './handler.filter';
+import { resolve } from 'path';
+import { config } from 'dotenv';
+
+config({ path: resolve(__dirname, '../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HandlerFilter());
-  await app.listen(3000);
+
+  await app.listen(process.env.SERVER_PORT);
 }
+
 bootstrap();
